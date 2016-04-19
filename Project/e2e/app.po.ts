6ed9aa8010fa42f160f61783d87a1ca5b3bb21d2
Carlos Sevilla <carlos@MacBook-Air-de-Carlos-2.local>
2016-04-19 09:06:53 +0200

    Borrado projecto 'Project' de angular-cli
    Borrado el projecto creado con angular-cli dado que finalmente usaremos el projecto base proporcionado por el profesor para su uso con Brackets.

--- Project/e2e/app.po.ts
+++
@@ -1,4 +0,0 @@
-export class ProjectPage {
-  navigateTo() { return browser.get('/'); }
-  getParagraphText() { return element(by.css('Project-app p')).getText(); }
-}
--- Project/src/app.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {ProjectApp} from './app/project';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-
-bootstrap(ProjectApp, [
-  ROUTER_PROVIDERS
-]);
